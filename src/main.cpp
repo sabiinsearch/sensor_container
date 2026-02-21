5d733183b02e588e105ba942a9f48f3961071ced
sabiinsearch <sabiinsearch@gmail.com>
2026-02-21 09:16:30 +0530

    Some_err

--- src/main.cpp
+++ src/main.cpp
@@ -1,102 +1,55 @@
-#include <Arduino.h>
-#include <esp_now.h>
-#include <WiFi.h>
+  // Change from 2048 to 4096 or higher
+// Libraries for AWS
+#include "secrets.h"             //  for AWS Certificates and Keys
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "WiFi.h"
 
-// my library
+// Others
+// #include <nvs.h>
+// #include <nvs_flash.h>
+
+
+// my libraries
+#include "appManager.h"
 #include "receiverBoard.h"
+#include "sensor.h"
 
-// Peer MAC Address
-uint8_t peerAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; // Replace
-
-esp_now_peer_info_t peerInfo;
-
-
-// Structure to send
-typedef struct struct_message {
-    int id;
-    float temp;
-} struct_message;
-
-struct_message myData;
+// my Managers
+appManager managr;
 
 
-// Send callback
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-  digitalWrite(HEARTBEAT_LED, HIGH);
-  delay(100);  
-  digitalWrite(HEARTBEAT_LED, LOW);
-}
+// DHT dht(DHTPIN, DHTTYPE);
 
-// Receive callback
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  memcpy(&myData, incomingData, sizeof(myData));
-  Serial.print("Bytes received: ");
-  Serial.println(len);
-  Serial.print("Temp: ");
-  Serial.println(myData.temp);
-  digitalWrite(HEARTBEAT_LED, HIGH);
-  delay(100);  
-  digitalWrite(HEARTBEAT_LED, LOW);
-}
+// WiFiClientSecure net = WiFiClientSecure();
+// PubSubClient client(net);
 
-void setupESP_NOW()
-  {
-    // Set device as a Wi-Fi Station
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();  
-    // Init ESP-NOW
-    if (esp_now_init() != 0) 
-      {
-        Serial.println("Error initializing ESP-NOW");
-        return;
-      }
-    // Set ESP-NOW Role
-    //esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
 
-    // Register for a callback function that will be called when data is received
-    esp_now_register_recv_cb(OnDataRecv);
-    esp_now_register_send_cb(OnDataSent);
 
-    memcpy(peerInfo.peer_addr, peerAddress, 6);
-    peerInfo.channel = 0;  
-    peerInfo.encrypt = false;
-    esp_now_add_peer(&peerInfo);
-    // Register peer
-    //esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
-    Serial.println("ESP-NOW setup done.");
-    delay(500);
-  }
+void setup()
+{
 
-  void intBoard() {
-    pinMode(HEARTBEAT_LED, OUTPUT);
-    digitalWrite(HEARTBEAT_LED, LOW);
-  }
+  // Change from 2048 to 4096 or higher
+//xTaskCreate(TaskFunction, "ScaleTask", 4096, NULL, 1, NULL);
 
-void setup() {
   Serial.begin(115200);
-  while (!Serial) {
-    delay(10);
-  }
-  delay(1000); // Extra delay for stability
+ 
+    // Initiating Manager
+  //Serial.println("Initializing App Manager..");
+  appManager_ctor(&managr);
   
-  intBoard();
-  setupESP_NOW();
+  Serial.println("All Systems Initialized..");
+
+ 
 }
 
-void loop() {
-    delay(1000); 
-    //Serial.println("In Loop");   
-    myData.id = 1;
-    myData.temp = 24.5;
-    esp_err_t result = esp_now_send(peerAddress, (uint8_t *) &myData, sizeof(myData));
-    if (result == ESP_OK) {
-
-        Serial.println("Sent with success");
-    } else {
-        Serial.println("Error sending the data");
-    }
-    delay(1000);
-
+void loop()
+{
+    
+  getSensorData_print_update(&managr);
+  
+  checkButtonPressed(&managr);
+  loop_mgr(&managr);
+  delay(100);
 }
