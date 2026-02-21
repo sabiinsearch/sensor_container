9bd9b7f619b28917510a8b74e4611a670f7d661c
sabiinsearch <sabiinsearch@gmail.com>
2026-02-21 12:58:45 +0530

    Some_err

--- lib/myCust_lib/appManager.cpp
+++ lib/myCust_lib/appManager.cpp
@@ -161,7 +161,8 @@ DHT dht(DHT_pin, DHT_type);   // DHT sensor object
       pref.begin("app_conf",false);
 
     // get threshold from cloud
-     if((pref.getFloat("threshold",0))==0) {      
+     if((pref.getFloat("threshold",0))==0) {  
+        pref.putFloat("threshold",0.0);    
         appMgr->load_threshold = 0.0;
      }
       else {
@@ -494,7 +495,7 @@ void getSensorData_print_update(appManager* appMgr) {
 
   appMgr->scale.power_up();
 
-  //initLoadCell(appMgr);
+  initLoadCell(appMgr);
   
   // Add timeout or delay to prevent infinite loop WDT reset
   long loop_start = millis();
